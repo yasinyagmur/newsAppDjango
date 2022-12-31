a3a2e3f7dc453bb0ce2de3a05f25f2bd2940877b
yasin yağmur <yasin.yagmur@gmail.com>
2022-12-31 20:05:17 +0300

    created serializer

--- news/models.py
+++ news/models.py
@@ -17,13 +17,13 @@ class Post(models.Model):
     category_id = models.ForeignKey(
         Category,related_name= 'blog_posts',on_delete=models.CASCADE
     )
-    status = models.BooleanField(blank=True,null=True)
+    status = models.BooleanField()
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     image = models.ImageField(blank=True,null=True,upload_to='newsImages')
 
     def __str__(self) -> str:
-        return f'{self.image}--{self.title}'
+        return f'{self.title}'
 
     class Meta:
         ordering = ('id',)
---
+++ news/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Category,Post
+
+
+class CategorySerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model =Category
+        fields = ['name']
+    
+    # blog_posts = CategorySerializers(many=True)
+    # blog_posts = serializers.HyperlinkedRelatedField(
+    #     many=True,
+    #     read_only =True,
+    #     view_name = 'post'
+    # )
+
+class PostSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = '__all__'
