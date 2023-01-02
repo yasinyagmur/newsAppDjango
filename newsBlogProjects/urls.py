2b215b4059534979181d19ddca35028078bbeac3
yasin yağmur <yasin.yagmur@gmail.com>
2023-01-02 20:29:56 +0300

    fixed

--- newsBlogProjects/urls.py
+++ newsBlogProjects/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include("news.urls")),
 ]
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
