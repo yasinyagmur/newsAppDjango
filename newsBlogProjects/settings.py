8a5f8b7ca498c54f51ba10fe087074c4110d87b4
yasin yağmur <yasin.yagmur@gmail.com>
2023-01-02 22:56:55 +0300

    create user app

--- newsBlogProjects/settings.py
+++ newsBlogProjects/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django_filters',
     # my apps
     'news',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -138,5 +139,7 @@ REST_FRAMEWORK = {
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
     # 'PAGE_SIZE': 30,
     # 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
 }
